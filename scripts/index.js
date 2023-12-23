99fd7f6ecf78f16b7c87480da69855f0ae9b1185
RTD404 <evgeniy_04_severov@mail.ru>
2023-12-23 19:04:27 +0300

    добавить функции добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,32 @@
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+function createCard(data, deleteCallback) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); 
 
+    cardElement.querySelector('.card__image').src = data.link;
+    cardElement.querySelector('.card__title').textContent = data.name;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function () {
+        deleteCard(cardElement);
+    });
+
+    return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+function addCardsToPage(cards) {
+    const placesList = document.querySelector('.places__list');
+  
+    cards.forEach(function (cardData) {
+      const card = createCard(cardData, deleteCard);
+  
+      placesList.appendChild(card);
+    });
+  }
+
+addCardsToPage(initialCards)
