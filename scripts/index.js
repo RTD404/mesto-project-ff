af0468e9299986be08203df0a31908274203cb41
RTD404 <evgeniy_04_severov@mail.ru>
2023-12-23 21:12:02 +0300

    исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 // @todo: Темплейт карточки
-
+const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
 function createCard(data, deleteCard) {
-    const cardTemplate = document.querySelector('#card-template').content;
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true); 
 
     cardElement.querySelector('.card__image').src = data.link;
+    cardElement.querySelector('.card__image').alt = data.name;
     cardElement.querySelector('.card__title').textContent = data.name;
 
     cardElement.querySelector('.card__delete-button').addEventListener('click', function () {
@@ -24,12 +25,11 @@ function deleteCard(cardElement) {
 
 // @todo: Вывести карточки на страницу
 function addCardsToPage(cards) {
-    const placesList = document.querySelector('.places__list');
   
     cards.forEach(function (cardData) {
       const card = createCard(cardData, deleteCard);
   
-      placesList.append(card);
+      cardsContainer.append(card);
     });
 }
 
